473a7dd390a5195fababe6404c15cfdc24a0e54a
Bankirov Aleksey <bankirovaleksey@gmail.com>
2019-06-06 23:20:18 +0300

    version 1.0

---
+++ public/404.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from demo.shapedtheme.com/kotha-pro-html/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 28 Apr 2019 08:46:58 GMT -->
+<head>
+    <!-- Document Settings -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <!-- Page Title -->
+    <title>link is outdated</title>
+    <!-- Fonts -->
+    <link
+            href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,700i%7cOswald:300,400,500,600,700%7cPlayfair+Display:400,400i,700,700i"
+            rel="stylesheet">
+    <!-- Styles -->
+    <link rel="stylesheet" href="/front/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/front/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/front/css/slick-theme.css">
+    <link rel="stylesheet" href="/front/css/slick.css">
+    <link rel="stylesheet" href="/front/css/style.css">
+    <!-- HTML5 shim and Respond.js IE9 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="/front/js/html5shiv.js"></script>
+    <script src="/front/js/respond.js"></script>
+    <![endif]-->
+</head>
+<body>
+<header class="kotha-menu marketing-menu">
+    <nav class="navbar  navbar-default">
+        <div class="container">
+            <div class="menu-content">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                            data-target="#myNavbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav text-uppercase pull-left">
+                        <li>
+                            <a href="index.html">Главная</a>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"  role="button"
+                               aria-haspopup="true" aria-expanded="false">Категории</a>
+                            <ul class="dropdown-menu">
+                                <li class="active"><a href="index-2.html">Home Default</a></li>
+                                <li class=""><a href="index-feminine.html">Home Feminine</a></li>
+                                <li class=""><a href="index-travel.html">Home Travel</a></li>
+                                <li class=""><a href="index-food.html">Home Food</a></li>
+                                <li class=""><a href="index-material.html">Home Material</a></li>
+                            </ul>
+                        <li><a href="contact.html">Contact</a></li>
+                        <li><a href="/admin.html">adminLTE</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+    <div class="kotha-logo text-center">
+        <h1><a href="index-2.html"><img src="/front/images/kotha-logo.png" alt="kothPro"></a></h1>
+    </div>
+</header>
+<div class="st-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div id="primary" class="content-area padding-content white-color">
+                    <main id="main" class="site-main"  >
+
+                        <section class="error-404 not-found text-center">
+                            <h2 class="404">404</h2>
+                            <p class="lead">К сожалению, мы не смогли найти страницу, которую вы ищете!</p>
+                            <div class="row">
+                                <div class="col-sm-4 col-sm-offset-4">
+                                    <form role="search" method="get" id="searchform" action="#">
+                                        <div>
+                                            <input type="text" placeholder="Search and hit enter..." name="s" id="s">
+                                        </div>
+                                    </form>
+                                    <p class="go-back-home"><a href="index.html">
+                                            Вернуться на главную страницу</a></p>
+                                </div>
+                            </div>
+                        </section><!-- .error-404 -->
+                    </main><!-- #main -->
+                </div><!-- #primary -->
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid insta-feed text-center">
+    <h2 class="text-uppercase">Follow@ <a href="#">Instagram</a></h2>
+    <div id="footer-instagram" class="footer-insta">
+        <div class="item">
+            <div class="single-instagram">
+                <a href="#">
+                    <img src="/front/images/ft-insta-1.jpg" alt="">
+                </a>
+                <div class="insta-overlay">
+                    <div class="insta-meta">
+                        <ul class="list-inline text-center">
+                            <li><a href="#"><i class="fa fa-heart-o"></i></a> 325</li>
+                            <li><a href="#"><i class="fa fa-comment-o"></i></a> 20</li>
+                        </ul>
+                    </div>
+                </div>
+                <a href="#" class="insta-link"></a>
+            </div>
+        </div>
+        <div class="item">
+            <div class="single-instagram">
+                <a href="#">
+                    <img src="/front/images/ft-insta-2.jpg" alt="">
+                </a>
+                <div class="insta-overlay">
+                    <div class="insta-meta">
+                        <ul class="list-inline text-center">
+                            <li><a href="#"><i class="fa fa-heart-o"></i></a> 325</li>
+                            <li><a href="#"><i class="fa fa-comment-o"></i></a> 20</li>
+                        </ul>
+                    </div>
+                </div>
+                <a href="#" class="insta-link"></a>
+            </div>
+
+        </div>
+        <div class="item">
+            <div class="single-instagram">
+                <a href="#">
+                    <img src="/front/images/ft-insta-3.jpg" alt="">
+                </a>
+                <div class="insta-overlay">
+                    <div class="insta-meta">
+                        <ul class="list-inline text-center">
+                            <li><a href="#"><i class="fa fa-heart-o"></i></a> 325</li>
+                            <li><a href="#"><i class="fa fa-comment-o"></i></a> 20</li>
+                        </ul>
+                    </div>
+                </div>
+                <a href="#" class="insta-link"></a>
+            </div>
+
+        </div>
+        <div class="item">
+            <div class="single-instagram">
+                <a href="#">
+                    <img src="/front/images/ft-insta-4.jpg" alt="">
+                </a>
+                <div class="insta-overlay">
+                    <div class="insta-meta">
+                        <ul class="list-inline text-center">
+                            <li><a href="#"><i class="fa fa-heart-o"></i></a> 325</li>
+                            <li><a href="#"><i class="fa fa-comment-o"></i></a> 20</li>
+                        </ul>
+                    </div>
+                </div>
+                <a href="#" class="insta-link"></a>
+            </div>
+        </div>
+        <div class="item">
+            <div class="single-instagram">
+                <a href="#">
+                    <img src="/front/images/ft-insta-5.jpg" alt="">
+                </a>
+                <div class="insta-overlay">
+                    <div class="insta-meta">
+                        <ul class="list-inline text-center">
+                            <li><a href="#"><i class="fa fa-heart-o"></i></a> 325</li>
+                            <li><a href="#"><i class="fa fa-comment-o"></i></a> 20</li>
+                        </ul>
+                    </div>
+                </div>
+                <a href="#" class="insta-link"></a>
+            </div>
+        </div>
+        <div class="item">
+            <div class="single-instagram">
+                <a href="#">
+                    <img src="/front/images/ft-insta-6.jpg" alt="">
+                </a>
+                <div class="insta-overlay">
+                    <div class="insta-meta">
+                        <ul class="list-inline text-center">
+                            <li><a href="#"><i class="fa fa-heart-o"></i></a> 325</li>
+                            <li><a href="#"><i class="fa fa-comment-o"></i></a> 20</li>
+                        </ul>
+                    </div>
+                </div>
+                <a href="#" class="insta-link"></a>
+            </div>
+        </div>
+        <div class="item">
+            <div class="single-instagram">
+                <a href="#">
+                    <img src="/front/images/ft-insta-7.jpg" alt="">
+                </a>
+                <div class="insta-overlay">
+                    <div class="insta-meta">
+                        <ul class="list-inline text-center">
+                            <li><a href="#"><i class="fa fa-heart-o"></i></a> 325</li>
+                            <li><a href="#"><i class="fa fa-comment-o"></i></a> 20</li>
+                        </ul>
+                    </div>
+                </div>
+                <a href="#" class="insta-link"></a>
+            </div>
+        </div>
+        <div class="item">
+            <div class="single-instagram">
+                <a href="#">
+                    <img src="/front/images/ft-insta-8.jpg" alt="">
+                </a>
+                <div class="insta-overlay">
+                    <div class="insta-meta">
+                        <ul class="list-inline text-center">
+                            <li><a href="#"><i class="fa fa-heart-o"></i></a> 325</li>
+                            <li><a href="#"><i class="fa fa-comment-o"></i></a> 20</li>
+                        </ul>
+                    </div>
+                </div>
+                <a href="#" class="insta-link"></a>
+            </div>
+        </div>
+    </div>
+</div>
+<footer>
+    <div class="container">
+        <div class="footer-widget-row">
+            <div class="footer-widget contact-widget">
+                <div class="widget-title">
+                    <img src="/front/images/ft-logo.png" alt="">
+                </div>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+                    ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eaccusam et justo duo
+                    dolores eteem.</p>
+                <div class="address">
+                    <h4 class="text-uppercase">contact Info</h4>
+                    <p> 239/2 NK Street, DC, USA</p>
+                    <p> Phone: +123 456 78900</p>
+                    <a href="mailto:theme@kotha.com">theme@kotha.com</a>
+                </div>
+            </div>
+            <div class="footer-widget twitter-widget">
+                <h2 class="widget-title text-uppercase">
+                    Latest TWeeTs
+                </h2>
+                <div class="single-tweet">
+                    <p>Check our new theme 'kotha - Personal WordPress Blog Theme' on #themeforest #Envato
+                        #WordPress <br>
+                        <a href="https://t.co/kN5OPEuPzx">https://t.co/kN5OPEuPzx</a></p>
+                    <h4><i class="fa fa-twitter"></i>Tweeted on 17 hours ago</h4>
+                </div>
+                <div class="single-tweet">
+                    <p>Check our new theme 'kotha - Personal WordPress Blog Theme' on #themeforest #Envato
+                        #WordPress
+                        <br>
+                        <a href="https://t.co/kN5OPEuPzx">https://t.co/kN5OPEuPzx</a></p>
+                    <h4><i class="fa fa-twitter"></i>Tweeted on 17 hours ago</h4>
+                </div>
+            </div>
+
+            <div class="footer-widget testimonial-widget">
+                <h2 class="widget-title text-uppercase">Testimonials</h2>
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <!--Indicator-->
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+                        <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+                    </ol>
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item">
+                            <div class="single-review">
+                                <div class="review-text">
+                                    <p>Lorem ipsum dolor sit amet, conssadipscing elitr, sed diam nonumy eirmod
+                                        tempvidunt ut labore et dolore magna aliquyam erat,sed diam voluptua. At
+                                        vero eos et accusam justo duo dolores et ea rebum.gubergren no sea takimata
+                                        magna aliquyam eratma</p>
+                                </div>
+                                <div class="author-id">
+                                    <img src="/front/images/author.png" alt="">
+                                    <div class="author-text">
+                                        <h4>Sophia</h4>
+                                        <h4>CEO, ReadyTheme</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item active">
+                            <div class="single-review">
+                                <div class="review-text">
+                                    <p>Lorem ipsum dolor sit amet, conssadipscing elitr, sed diam nonumy eirmod
+                                        tempvidunt ut labore et dolore magna aliquyam erat,sed diam voluptua. At
+                                        vero eos et accusam justo duo dolores et ea rebum.gubergren no sea takimata
+                                        magna aliquyam eratma</p>
+                                </div>
+                                <div class="author-id">
+                                    <img src="/front/images/author.png" alt="">
+                                    <div class="author-text">
+                                        <h4>Sophia</h4>
+                                        <h4>CEO, ReadyTheme</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="single-review">
+                                <div class="review-text">
+                                    <p>Lorem ipsum dolor sit amet, conssadipscing elitr, sed diam nonumy eirmod
+                                        tempvidunt ut labore et dolore magna aliquyam erat,sed diam voluptua. At
+                                        vero eos et accusam justo duo dolores et ea rebum.gubergren no sea takimata
+                                        magna aliquyam eratma</p>
+                                </div>
+                                <div class="author-id">
+                                    <img src="/front/images/author.png" alt="">
+
+                                    <div class="author-text">
+                                        <h4>Sophia</h4>
+                                        <h4>CEO, ReadyTheme</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid text-center ft-copyright">
+        <p>&copy;  2017   <a href="#">Kotha PRO </a> - Designed with <i class="fa fa-heart"></i> by <a href="http://shapedtheme.com/">ShapedTheme</a></p>
+    </div>
+</footer>
+<div class="scroll-up">
+    <a href="#"><i class="fa fa-angle-up"></i></a>
+</div>
+<!--//Script//-->
+<script src="/front/js/jquery-1.12.4.min.js"></script>
+<script src="/front/js/bootstrap.min.js"></script>
+<script src="/front/js/slick.min.js"></script>
+<script src="/front/js/main.js"></script>
+</body>
+
+<!-- Mirrored from demo.shapedtheme.com/kotha-pro-html/404.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 28 Apr 2019 08:50:25 GMT -->
+</html>
---
+++ public/index.php
@@ -0,0 +1,108 @@
+<?php
+// Start a Session
+if( !session_id() ) @session_start();
+
+require '../vendor/autoload.php';
+
+$contBuilder = new DI\ContainerBuilder;
+
+$contBuilder->addDefinitions([
+    League\Plates\Engine::class => function(){
+        return new League\Plates\Engine('../app/views');
+    },
+
+    \PDO::class => function(){
+        $driver = 'mysql';
+        $host = 'localhost';
+        $dbname = 'myblog';
+        $user = 'root';
+        $password = '';
+
+        $Options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        return new \PDO("$driver:host=$host;dbname=$dbname", $user, $password, $Options);
+    },
+
+    Delight\Auth\Auth::class => function($container){
+        return new Delight\Auth\Auth($container->get('PDO'), null, null, false);
+    },
+
+    Aura\SqlQuery\QueryFactory::class => function(){
+        return new Aura\SqlQuery\QueryFactory('mysql');
+    },
+
+    Intervention\Image\ImageManager::class => function(){
+        return new Intervention\Image\ImageManager(array('driver' => 'imagick'));
+    }
+]);
+
+$container = $contBuilder->build();
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', ['App\controllers\HomeController', 'index']);
+    $r->addRoute('GET', '/index.html[?page={id:\d+}]', ['App\controllers\HomeController', 'index']);
+    $r->addRoute('GET', '/login.html', ['App\controllers\UserController', 'login']);
+    $r->addRoute('POST', '/validation-login.html', ['App\controllers\UserController', 'validationLogin']);
+    $r->addRoute('GET', '/register.html', ['App\controllers\UserController', 'register']);
+    $r->addRoute('POST', '/validation-register.html', ['App\controllers\UserController', 'validationRegister']);
+    $r->addRoute('GET', '/verification-email.html', ['App\controllers\UserController', 'verificationEmail']);
+    $r->addRoute('GET', '/logout.html', ['App\controllers\UserController', 'logout']);
+    $r->addRoute('GET', '/contact.html', ['App\controllers\HomeController', 'contact']);
+    $r->addRoute('GET', '/show.html[?id={id:\d+}]', ['App\controllers\HomeController', 'show']);
+    //comment
+    $r->addRoute('POST', '/add-comment.html[?id={id:\d+}]', ['App\controllers\CommentController', 'addComment']);
+
+    //Admin Routes
+    $r->addRoute('GET', '/admin.html', ['App\controllers\admin\HomeController', 'index']);
+    $r->addRoute('GET', '/admin/categories/index.html', ['App\controllers\admin\HomeController', 'categories']);
+    $r->addRoute('GET', '/admin/categories/create-category.html', ['App\controllers\admin\CategoryController', 'formAddCategory']);
+    $r->addRoute('POST', '/admin/categories/add-category.html', ['App\controllers\admin\CategoryController', 'addCategory']);
+    $r->addRoute('GET', '/admin/categories/edit.html[?id={id:\d+}]', ['App\controllers\admin\CategoryController', 'editForm']);
+    $r->addRoute('POST', '/admin/categories/update.html', ['App\controllers\admin\CategoryController', 'update']);
+    $r->addRoute('GET', '/admin/categories/delete.html[?id={id:\d+}]', ['App\controllers\admin\CategoryController', 'delete']);
+    //user
+    $r->addRoute('GET', '/admin/users/index.html', ['App\controllers\admin\UserController', 'users']);
+    $r->addRoute('GET', '/admin/users/create.html', ['App\controllers\admin\UserController', 'showUserCreationForm']);
+    $r->addRoute('GET', '/admin/users/edit.html', ['App\controllers\admin\UserController', 'showEditForm']);
+    $r->addRoute('GET', '/admin/users/update.html[?id={id:\d+}]', ['App\controllers\admin\UserController', 'update']);
+    $r->addRoute('POST', '/admin/users/create-user.html', ['App\controllers\admin\UserController', 'createUser']);
+    $r->addRoute('GET', '/admin/pages/index.html', ['App\controllers\admin\HomeController', 'pages']);
+    //fotos
+    $r->addRoute('GET', '/admin/photos/index.html', ['App\controllers\admin\PhotoController', 'index']);
+    $r->addRoute('GET', '/admin/photos/create.html', ['App\controllers\admin\PhotoController', 'formCreate']);
+    $r->addRoute('POST', '/admin/photos/create-photo.html', ['App\controllers\admin\PhotoController', 'create']);
+
+    //post
+    $r->addRoute('GET', '/admin/post/create.html', ['App\controllers\admin\PostController', 'create']);
+    $r->addRoute('POST', '/admin/post/create-post.html', ['App\controllers\admin\PostController', 'addPost']);
+});
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        include '404.php';
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        echo "Метод $allowedMethods не разрешон";
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[1];
+        $cont = $container->call($routeInfo[1], $routeInfo[2]);
+        break;
+}
